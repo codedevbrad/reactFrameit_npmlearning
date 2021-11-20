1053522b213a652c413bf8d89b0343219d59ff1d
codedevbrad <codedevbrad@gmail.com>
2021-11-20 18:52:06 +0000

    initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# @codedevbrad/testlibrary
+
+learning how to upload react component libraries to NPM.
+
+## link to library
+
+https://www.npmjs.com/package/@codedevbrad/testlibrary
---
+++ library/index.js
@@ -0,0 +1,7 @@
+import Button from './button';
+import Header from './header';
+import useComponentVisible from './useClick';
+
+export { 
+    Button, Header, useComponentVisible
+};
